41e13ac3bbb96f6c342274e05518520b729489c8
Swati Jain <jain.swati71@gmail.com>
2024-03-05 15:01:01 +0100

    Added styling and toggling panel collapse logic

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,24 +1,37 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
+    const handleClick = (nextIndex) => {
+        if (expandedIndex === nextIndex) {
+            setExpandedIndex(-1);
+        }else {
+        setExpandedIndex(nextIndex);
+        }
+    }
 
 
     const renderedItems = items.map((item, index) => {
        const isExpanded = index === expandedIndex; 
 
+       const icon = <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+       </span> 
+
         return (
             <div key={item.id}>
-                <div onClick={() => setExpandedIndex(index)}>
+                <div className="flex p-3 bg-gray-50 border-b items-center cursor-pointer justify-between" onClick={() => handleClick(index)}>
                     {item.label}
+                    {icon}
                 </div>
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         );
     });
     
-    return <div>
+    return <div className="border-x border-t rounded">
         {renderedItems}
     </div>;
 
