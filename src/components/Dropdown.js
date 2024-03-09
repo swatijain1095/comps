1523fd168cf5c245edbe331cc9209d6febf477ed
Swati Jain <jain.swati71@gmail.com>
2024-03-09 13:23:33 +0100

    Added changes related to useRef in dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { GoChevronDown } from 'react-icons/go';
 import Panel from "./Panel";
 
 function Dropdown({ options, value , onChange }) {
     const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (!divEl.current.contains(event.target)){
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener('click', handler, true);
+        return () => {
+            document.removeEventListener('click', handler);
+        }
+    }, []); 
 
     const handleClick = () => {
         setIsOpen(!isOpen);
@@ -18,7 +31,7 @@ function Dropdown({ options, value , onChange }) {
         return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
     });
     return (
-        <div className="w-48 relative">
+        <div ref={divEl} className="w-48 relative">
             <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
             {value?.label || 'Select...'}
             <GoChevronDown className="text-lg"/>
