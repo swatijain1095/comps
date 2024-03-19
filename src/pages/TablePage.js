1d565712c970cd3fa6c52852291802d60d0d4806
Swati Jain <jain.swati71@gmail.com>
2024-03-19 22:27:06 +0100

    Added sorting logic on table

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
 
@@ -13,14 +14,16 @@ function TablePage() {
             { 
                 label: 'Name',
                 render: (fruit) => fruit.name,
+                sortValue: (fruit) => fruit.name
             },
             { 
                 label: 'Color',
-                render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+                render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />
             },
             { 
                 label: 'Score',
                 render: (fruit) => fruit.score,
+                sortValue: (fruit) => fruit.score
             },
         ];
 
@@ -30,7 +33,7 @@ function TablePage() {
 
     return (
         <div>
-            <Table data={data} config={config} keyFn={keyFn}/>
+            <SortableTable data={data} config={config} keyFn={keyFn}/>
         </div>
     );
 }
